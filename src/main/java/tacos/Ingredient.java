9890d714edaeb806614853f369f837127212f90c
topdev21 <tcpint21@naver.com>
2025-05-03 16:13:59 +0900

    Applying JPA to domain objects.

--- src/main/java/tacos/Ingredient.java
+++ src/main/java/tacos/Ingredient.java
@@ -1,32 +1,25 @@
 package tacos;
 
 import org.springframework.data.annotation.Id;
-import org.springframework.data.domain.Persistable;
-import org.springframework.data.relational.core.mapping.Table;
 
+import jakarta.persistence.Entity;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@Table
+@Entity
 @AllArgsConstructor
-//@NoArgsConstructor(access=AccessLevel.PRIVATE, force=true)
-public class Ingredient implements Persistable<String>{
+@NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
+public class Ingredient {
 	@Id
-	private final String id;
-	private final String name;
-	private final Type type;
+	private String id;
+	private String name;
+	private Type type;
 
-	public enum Type{
+	public enum Type {
 		WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
-	}
-
-	@Override
-	public boolean isNew() {
-		// TODO Auto-generated method stub
-		return false;
 	}
 
 }
--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -4,17 +4,21 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Table;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.ManyToMany;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
-@Table
+@Entity
 public class Taco {
-	
+
 	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Long id;
 	private Date createdAt = new Date();
 
@@ -22,7 +26,7 @@ public class Taco {
 	@Size(min = 5, message = "Name must be at least 5 characters long")
 	private String name;
 
-	@NotNull
 	@Size(min = 1, message = "You must choose at least 1 ingredient")
+	@ManyToMany()
 	private List<Ingredient> ingredients;
 }
